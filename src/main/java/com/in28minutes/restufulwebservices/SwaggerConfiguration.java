b99530d91f434a04f7df49783261cee7e5fe5dfe
aravannai <38413795+aravannai@users.noreply.github.com>
2019-12-18 07:50:01 +0530

    Added swagger logic

--- src/main/java/com/in28minutes/restufulwebservices/SwaggerConfiguration.java
+++ src/main/java/com/in28minutes/restufulwebservices/SwaggerConfiguration.java
@@ -3,6 +3,8 @@ package com.in28minutes.restufulwebservices;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -11,8 +13,12 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 public class SwaggerConfiguration {
 
+	public static final Contact DEFAULT_CONTACT = new Contact("Sathish Dharanidharan", "www.sathish.com", "Restful WebService");
+	public static final ApiInfo DEFAULT_API_INFO = new ApiInfo("Api Documentation", "Api Documentation", "1.0", "urn:tos",
+	          DEFAULT_CONTACT, "Apache 2.0", "http://www.apache.org/licenses/LICENSE-2.0");;
+
 	@Bean
 	public Docket api() {
-		return new Docket(DocumentationType.SWAGGER_2);
+		return new Docket(DocumentationType.SWAGGER_2).apiInfo(DEFAULT_API_INFO);
 	}
 }
