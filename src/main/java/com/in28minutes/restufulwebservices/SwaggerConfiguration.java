c834813b930bc702a7808b94749bf9b15dd3f987
aravannai <38413795+aravannai@users.noreply.github.com>
2019-12-23 07:50:23 +0530

    FixForSwagger

--- src/main/java/com/in28minutes/restufulwebservices/SwaggerConfiguration.java
+++ src/main/java/com/in28minutes/restufulwebservices/SwaggerConfiguration.java
@@ -1,5 +1,9 @@
 package com.in28minutes.restufulwebservices;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -15,10 +19,13 @@ public class SwaggerConfiguration {
 
 	public static final Contact DEFAULT_CONTACT = new Contact("Sathish Dharanidharan", "www.sathish.com", "Restful WebService");
 	public static final ApiInfo DEFAULT_API_INFO = new ApiInfo("Api Documentation", "Api Documentation", "1.0", "urn:tos",
-	          DEFAULT_CONTACT, "Apache 2.0", "http://www.apache.org/licenses/LICENSE-2.0");;
+	          DEFAULT_CONTACT, "Apache 2.0", "http://www.apache.org/licenses/LICENSE-2.0");
+	public static final Set<String> DEFAULT_PRODUCES_CONSUMES = new HashSet<String>(Arrays.asList("application/json", "application/xml"));
 
 	@Bean
 	public Docket api() {
-		return new Docket(DocumentationType.SWAGGER_2).apiInfo(DEFAULT_API_INFO);
+		return new Docket(DocumentationType.SWAGGER_2).apiInfo(DEFAULT_API_INFO)
+				.produces(DEFAULT_PRODUCES_CONSUMES)
+				.consumes(DEFAULT_PRODUCES_CONSUMES);
 	}
 }
--- src/main/java/com/in28minutes/restufulwebservices/user/User.java
+++ src/main/java/com/in28minutes/restufulwebservices/user/User.java
@@ -5,12 +5,18 @@ import java.util.Date;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
+@ApiModel(description="All details of user")
 public class User {
 
 	private Integer id;
 	@Size(min=2, message="Name should have atleast 2 character")
+	@ApiModelProperty(notes="Name should have atleast 2 character")
 	private String name;
 	@Past
+	@ApiModelProperty(notes="Birth Date should be in past")
 	private Date birthDate;
 	
 	protected User() {
